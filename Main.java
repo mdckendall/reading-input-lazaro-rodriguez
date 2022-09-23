662eb2f004a86dd87e22e5aa405f8034295385e0
lazaro20 <113481833+lazaro-rodriguez@users.noreply.github.com>
2022-09-23 22:36:22 +0000

    Added the scanner to return the number of letters in the word

--- Main.java
+++ Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+  
+    Scanner scanner = new Scanner(System.in);
     System.out.println("Enter a word:");
+     String word = scanner.nextLine();
+
+    System.out.println(word.length());
+    
+
+    
+    
+
     
     //Your code will return the length of the entered String
   }
